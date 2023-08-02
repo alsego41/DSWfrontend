4bb242d6e1cd777eda430cd4c7df4cc6a2a163c9
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-02 20:20:19 -0300

    Importados componentes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/header.component';
+import { SearchComponent } from './search/search.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  imports: [HeaderComponent, SearchComponent],
 })
 export class AppComponent {
   title = 'dswfe';
