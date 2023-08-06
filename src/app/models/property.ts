895e8171d0390b2264c2981c4f27138b8ea6f40e
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-06 18:55:51 -0300

    Creada interfaces

---
+++ src/app/models/property.ts
@@ -0,0 +1,11 @@
+export interface Property {
+    idProperty: Number
+    statusProperty: String
+    address: String
+    zone: String
+    spaces: Number
+    roomQty: Number
+    bathQty: Number
+    backyard: Boolean
+    grill: Boolean
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,8 @@
+export interface User {
+    idUser: Number
+    firstName: String
+    lastName: String
+    dni: Number
+    email: String
+    bankAccount: String
+}
