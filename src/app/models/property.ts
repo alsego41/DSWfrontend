ebaea9c6dd281d3d9f910097c5618ddde3ebc264
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-06 19:52:46 -0300

    Cards ya usa mock de instancia

--- src/app/models/property.ts
+++ src/app/models/property.ts
@@ -1,8 +1,11 @@
 export interface Property {
     idProperty: Number
+    nameProperty: String
     statusProperty: String
+    photo: String
     address: String
     zone: String
+    m2: Number
     spaces: Number
     roomQty: Number
     bathQty: Number
