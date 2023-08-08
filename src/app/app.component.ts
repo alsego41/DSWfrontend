91dc76be02ec1098711e634d35d1079ae62d5c74
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-08 20:14:38 -0300

    Cards muestra info dinamica
    Ahora Cards tiene un for para mostrar todas las casas en un array.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,77 @@ import { Property } from './models/property';
 export class AppComponent {
   title = 'dswfe';
 
-  testProperty: Property = {
-    idProperty: 102,
-    nameProperty: "Casa elegante Barrio España",
-    statusProperty: "Disponible",
-    photo: "./assets/testcasa.jpg",
-    address: "Buenos Aires 132",
-    zone: "España",
-    m2: 40,
-    spaces: 3,
-    roomQty: 2,
-    bathQty: 1,
-    backyard: true,
-    grill: false
-  }
+  testProperties: Property[] = [
+    {
+      idProperty: 102,
+      nameProperty: "Casa elegante Barrio España",
+      statusProperty: "Disponible",
+      photo: "./assets/testcasa.jpg",
+      address: "Buenos Aires 132",
+      zone: "España",
+      m2: 40,
+      spaces: 3,
+      roomQty: 2,
+      bathQty: 1,
+      backyard: true,
+      grill: false
+    },
+    {
+      idProperty: 103,
+      nameProperty: "Casa elegante Barrio Martin",
+      statusProperty: "Disponible",
+      photo: "./assets/testcasa.jpg",
+      address: "Buenos Aires 132",
+      zone: "Martin",
+      m2: 50,
+      spaces: 3,
+      roomQty: 2,
+      bathQty: 1,
+      backyard: true,
+      grill: false
+    },
+    {
+      idProperty: 104,
+      nameProperty: "Casa elegante Barrio Banana",
+      statusProperty: "Disponible",
+      photo: "./assets/testcasa.jpg",
+      address: "Buenos Aires 132",
+      zone: "Banana",
+      m2: 30,
+      spaces: 3,
+      roomQty: 2,
+      bathQty: 1,
+      backyard: true,
+      grill: false
+    },
+    {
+      idProperty: 105,
+      nameProperty: "Casa elegante Barrio Urquiza",
+      statusProperty: "Disponible",
+      photo: "./assets/testcasa.jpg",
+      address: "Buenos Aires 132",
+      zone: "Urquiza",
+      m2: 40,
+      spaces: 3,
+      roomQty: 2,
+      bathQty: 1,
+      backyard: true,
+      grill: false
+    },
+    {
+      idProperty: 106,
+      nameProperty: "Casa elegante Barrio Chascomus",
+      statusProperty: "Disponible",
+      photo: "./assets/testcasa.jpg",
+      address: "Buenos Aires 132",
+      zone: "Chascomus",
+      m2: 40,
+      spaces: 3,
+      roomQty: 2,
+      bathQty: 1,
+      backyard: true,
+      grill: false
+    },
+  ]
+  
 }
