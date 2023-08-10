79d1f8fbcd7464a029ea5c6ef9ca44c47e59ba0a
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-10 19:10:11 -0300

    Actualizado para Router Outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PropertyDetailsComponent } from './components/property-details/property-details.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: "",
+    component: HomeComponent
+  },
+  {
+    path: "property", 
+    component: PropertyDetailsComponent, 
+    pathMatch: 'full'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
+
 export class AppRoutingModule { }
