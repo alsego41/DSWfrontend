eb5a9ec130a3febcc02a05f0609a9cec314337d4
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-08-10 20:58:06 -0300

    Creado Material Module
    Module especifico para todos los componentes de Material Angular.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
 import { SearchComponent } from './components/search/search.component';
@@ -10,24 +12,12 @@ import { CardsComponent } from './components/cards/cards.component';
 import { FooterComponent } from './components/footer/footer.component';
 import { FilterComponent } from './components/filter/filter.component';
 import { PropertyDetailsComponent } from './components/property-details/property-details.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle'
-import { MatAutocompleteModule } from '@angular/material/autocomplete'
-import { MatIconModule } from '@angular/material/icon';
-import {MatCardModule} from '@angular/material/card';
-import {MatButtonModule} from '@angular/material/button';
-import {MatChipsModule} from '@angular/material/chips';
-
 import { HomeComponent } from './components/home/home.component';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatNativeDateModule } from '@angular/material/core';
-import{MatInputModule} from '@angular/material/input';
-import{MatSelectModule} from '@angular/material/select';
-import{MatRadioModule} from '@angular/material/radio';
-import{MatDatepickerModule} from '@angular/material/datepicker';
 import { UserComponent } from './components/user/user.component';
+
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+
+import { MaterialModule } from './material.module';
 
 @NgModule({
   declarations: [
@@ -45,23 +35,9 @@ import { UserComponent } from './components/user/user.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatSlideToggleModule,
-    MatAutocompleteModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatChipsModule,
     FormsModule,
     ReactiveFormsModule,
-    MatFormFieldModule,
-    MatInputModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatCardModule,
-    MatButtonModule,
-    MatDatepickerModule,
-    MatNativeDateModule,
-
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
