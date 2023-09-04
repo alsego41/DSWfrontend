75000389aec43ccca34342be4b27dfc0033e5d78
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-03 22:48:47 -0300

    Agregada guards y reestructurado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,6 +8,7 @@ import { ContactComponent } from './components/contact/contact.component';
 import { RegisterUserComponent } from './components/register-user/register-user.component';
 import { NewPropertyComponent } from './components/new-property/new-property.component';
 import { LoginComponent } from './components/login/login.component';
+import { authGuard } from './auth/auth.guard';
 
 const routes: Routes = [
   {
@@ -22,32 +23,37 @@ const routes: Routes = [
   {
     path: "user",
     component: UserComponent,
+    canActivate: [authGuard],
     children: [
       {
-        path: "register",
-        component: RegisterUserComponent,
-        // pathMatch: "full"
+        path: "properties",
+        // component: PropertyListComponent
+        component: UserComponent
       },
       {
-        path: "login",
-        component: LoginComponent
-      },
-      // {
-      //   path: "",
-      //   component: UserDashboardComponent
-      // }
+        path: "properties/new",
+        component: NewPropertyComponent
+      }
     ]
+  },
+  {
+    path: "register",
+    component: RegisterUserComponent,
+  },
+  {
+    path: "login",
+    component: LoginComponent
   },
   // {
   //   path: "user/properties",
   //   component: PropertyListComponent,
   //   pathMatch: "full"
   // },
-  {
-    path: "user/properties/new",
-    component: NewPropertyComponent,
-    pathMatch: "full"
-  },
+  // {
+  //   path: "user/properties/new",
+  //   component: NewPropertyComponent,
+  //   pathMatch: "full"
+  // },
   {
     path: "about-us",
     component: AboutUsComponent,
