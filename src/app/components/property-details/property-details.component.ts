a5575d192c7b4155f073ba737c1718b73eb4d5c4
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-04 19:25:03 -0300

    Adaptado a _id

--- src/app/components/property-details/property-details.component.ts
+++ src/app/components/property-details/property-details.component.ts
@@ -1,21 +1,23 @@
-import { Component, inject } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { PropertyService } from '../../services/property.service';
-import { Property } from '../../models/property';
+import { Component, inject } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { PropertyService } from '../../services/property.service'
+import { Property } from '../../models/property'
 
 @Component({
-  selector: 'app-property-details',
-  templateUrl: './property-details.component.html',
-  styleUrls: ['./property-details.component.scss']
+	selector: 'app-property-details',
+	templateUrl: './property-details.component.html',
+	styleUrls: ['./property-details.component.scss'],
 })
 export class PropertyDetailsComponent {
-  route: ActivatedRoute = inject(ActivatedRoute)
-  propertyId = -1
-  propertyService: PropertyService = inject(PropertyService)
-  Property: Property | undefined
+	route: ActivatedRoute = inject(ActivatedRoute)
+	propertyId: String
+	propertyService: PropertyService = inject(PropertyService)
+	Property: Property | undefined
 
-  constructor(){
-    this.propertyId = Number(this.route.snapshot.params['id'])
-    this.propertyService.getPropertyById(this.propertyId).subscribe(data => this.Property = data)
-  }
+	constructor() {
+		this.propertyId = this.route.snapshot.params['id']
+		this.propertyService
+			.getPropertyById(this.propertyId)
+			.subscribe((data) => (this.Property = data))
+	}
 }
