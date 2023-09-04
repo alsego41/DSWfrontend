7a4875d70fafff4b6b6063660bae1e7a3bdccd42
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-04 19:24:33 -0300

    Reemplazo por service createProperty

--- src/app/components/new-property/new-property.component.ts
+++ src/app/components/new-property/new-property.component.ts
@@ -1,34 +1,50 @@
-import { Component } from '@angular/core';
-import { FormBuilder } from '@angular/forms';
+import { Component, inject } from '@angular/core'
+import { FormBuilder } from '@angular/forms'
+import { PropertyService } from 'src/app/services/property.service'
+import { Property } from 'src/app/models/property'
 
 @Component({
-  selector: 'app-new-property',
-  templateUrl: './new-property.component.html',
-  styleUrls: ['./new-property.component.scss']
+	selector: 'app-new-property',
+	templateUrl: './new-property.component.html',
+	styleUrls: ['./new-property.component.scss'],
 })
 export class NewPropertyComponent {
-  constructor(private formBuilder: FormBuilder) {}
-  newPropertyForm = this.formBuilder.group({
-    nameProperty: [''],
-    address: [''],
-    zone: [''],
-    m2: [''],
-    spaces: [''],
-    roomQty: [''],
-    bathQty: [''],
-    backyard: [''],
-    grill: ['']
-  })
-  newPropertyUrl = 'http://localhost:3000/property/new'
-  onSubmit() {
-    console.log(this.newPropertyForm.value)
-    fetch(this.newPropertyUrl, {
-      method: "POST",
-      body: JSON.stringify(this.newPropertyForm.value),
-      headers: {"Content-type": "application/json; charset=UTF-8"}
-    })
-      .then(res => res.json())
-      .then(json => console.log(json))
-      .catch(err => console.log(err))
-  }
+	constructor(private formBuilder: FormBuilder) {}
+	newPropertyForm = this.formBuilder.group({
+		nameProperty: [''],
+		address: [''],
+		zone: [''],
+		m2: [''],
+		spaces: [''],
+		roomQty: [''],
+		bathQty: [''],
+		backyard: [''],
+		grill: [''],
+	})
+	newProperty: Property
+	propertyService: PropertyService = inject(PropertyService)
+	onSubmit() {
+		this.newProperty = {
+			_id: '',
+			nameProperty: this.newPropertyForm.value.nameProperty || '',
+			statusProperty: '',
+			photo: '',
+			address: this.newPropertyForm.value.address as string,
+			zone: this.newPropertyForm.value.zone as string,
+			m2: Number(this.newPropertyForm.value.m2),
+			spaces: Number(this.newPropertyForm.value.spaces),
+			roomQty: Number(this.newPropertyForm.value.roomQty),
+			bathQty: Number(this.newPropertyForm.value.bathQty),
+			backyard: Boolean(this.newPropertyForm.value.backyard),
+			grill: Boolean(this.newPropertyForm.value.grill),
+		}
+		this.propertyService.createProperty(this.newProperty).subscribe({
+			next(res) {
+				console.log(`Property ${res._id} creada`)
+			},
+			error(err) {
+				console.log(err)
+			},
+		})
+	}
 }
