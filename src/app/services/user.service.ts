fd760a17aed5abcbe384c3727d0b9dbb8a53cee8
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-04 22:37:06 -0300

    Nuevo metodo para agregar propiedad a user

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,17 +1,23 @@
-import { Injectable } from '@angular/core';
-import { HttpClient, HttpResponse } from '@angular/common/http';
-import { Observable } from 'rxjs';
-import { LoginAuth, LoginBody } from '../models/login-auth';
+import { Injectable } from '@angular/core'
+import { HttpClient, HttpResponse } from '@angular/common/http'
+import { Observable } from 'rxjs'
+import { LoginAuth, LoginBody } from '../models/login-auth'
 
 @Injectable({
-  providedIn: 'root'
+	providedIn: 'root',
 })
 export class UserService {
+	constructor(private http: HttpClient) {}
+	baseUrl: String = 'http://localhost:3000'
 
-  constructor(private http: HttpClient) { }
-  baseUrl: String = 'http://localhost:3000'
+	login(body: LoginBody): Observable<LoginAuth> {
+		return this.http.post<LoginAuth>(`${this.baseUrl}/user/login`, body)
+	}
 
-  login(body: LoginBody) : Observable<LoginAuth> {
-    return this.http.post<LoginAuth>(`${this.baseUrl}/user/login`, body)
-  }
+	appendNewProperty(idProperty: String, token: String): Observable<Object> {
+		return this.http.patch<Object>(`${this.baseUrl}/user/properties/new`, {
+			idProperty,
+			token,
+		})
+	}
 }
