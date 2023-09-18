a302c66678226129e7cff2078c21e86dc8bd2b57
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-09-18 18:38:24 -0300

    added snackbarmodule

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,37 +1,38 @@
-import { NgModule } from '@angular/core';
-import { MatNativeDateModule } from '@angular/material/core';
+import { NgModule } from '@angular/core'
+import { MatNativeDateModule } from '@angular/material/core'
 
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatInputModule } from '@angular/material/input';
-import { MatSelectModule } from '@angular/material/select';
+import { MatFormFieldModule } from '@angular/material/form-field'
+import { MatInputModule } from '@angular/material/input'
+import { MatSelectModule } from '@angular/material/select'
 
-import { MatRadioModule } from '@angular/material/radio';
-import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatRadioModule } from '@angular/material/radio'
+import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatSlideToggleModule } from '@angular/material/slide-toggle'
 import { MatAutocompleteModule } from '@angular/material/autocomplete'
-import { MatIconModule } from '@angular/material/icon';
-import { MatCardModule } from '@angular/material/card';
-import { MatButtonModule } from '@angular/material/button';
-import { MatChipsModule } from '@angular/material/chips';
+import { MatIconModule } from '@angular/material/icon'
+import { MatCardModule } from '@angular/material/card'
+import { MatButtonModule } from '@angular/material/button'
+import { MatChipsModule } from '@angular/material/chips'
+import { MatSnackBarModule } from '@angular/material/snack-bar'
 
 const Material: any[] = [
-    MatFormFieldModule,
-    MatNativeDateModule,
-    MatInputModule,
-    MatSelectModule,
-    MatRadioModule,
-    MatDatepickerModule,
-    MatSlideToggleModule,
-    MatAutocompleteModule,
-    MatIconModule,
-    MatCardModule,
-    MatButtonModule,
-    MatChipsModule
+	MatFormFieldModule,
+	MatNativeDateModule,
+	MatInputModule,
+	MatSelectModule,
+	MatRadioModule,
+	MatDatepickerModule,
+	MatSlideToggleModule,
+	MatAutocompleteModule,
+	MatIconModule,
+	MatCardModule,
+	MatButtonModule,
+	MatChipsModule,
+	MatSnackBarModule,
 ]
 
 @NgModule({
-    imports: [Material],
-    exports: [Material]
+	imports: [Material],
+	exports: [Material],
 })
-
 export class MaterialModule {}
