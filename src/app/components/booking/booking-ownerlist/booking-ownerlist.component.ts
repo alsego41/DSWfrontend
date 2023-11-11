a0fd30dc2f030182bd9861a086c5b1634a6ac4f8
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-11-11 15:46:43 -0300

    Creada lista de bookings del owner

---
+++ src/app/components/booking/booking-ownerlist/booking-ownerlist.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-booking-ownerlist',
+  templateUrl: './booking-ownerlist.component.html',
+  styleUrls: ['./booking-ownerlist.component.scss']
+})
+export class BookingOwnerlistComponent {
+
+}
