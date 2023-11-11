1319e0b33889b58da13779962a2849c7adcd132e
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-11-11 15:45:36 -0300

    getUserBookings y createBooking

---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,36 @@
+import { Injectable } from '@angular/core'
+import { Booking } from '../models/booking.js'
+import { HttpClient } from '@angular/common/http'
+import { Observable } from 'rxjs'
+
+@Injectable({
+	providedIn: 'root',
+})
+export class BookingService {
+	private baseUrl: string =
+		(import.meta.env.NG_APP_API_BASE_URL as string) || 'http://localhost:3000'
+
+	constructor(private http: HttpClient) {}
+
+	getUserBookings(): Observable<Booking[]> {
+		const token = localStorage.getItem('token') || ''
+		return this.http.get<Booking[]>(`${this.baseUrl}/booking/guest`, {
+			headers: { Authorization: `Bearer ${token}` },
+		})
+	}
+
+	createBooking(info: {
+		checkIn: Date
+		checkOut: Date
+		propertyId: string
+	}): Observable<Booking> {
+		const token = localStorage.getItem('token') || ''
+		return this.http.post<Booking>(`${this.baseUrl}/booking/new`, {
+			booking: {
+				checkIn: info.checkIn,
+				checkOut: info.checkOut,
+			},
+			token,
+		})
+	}
+}
