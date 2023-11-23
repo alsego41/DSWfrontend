3e413304d57dce04777da5f922205602e7cc8944
Alberto Sebastián González <68853763+alsego41@users.noreply.github.com>
2023-11-23 01:34:35 -0300

    Updated visuals de detail

--- src/app/components/property/property-details/property-details.component.ts
+++ src/app/components/property/property-details/property-details.component.ts
@@ -15,6 +15,7 @@ export class PropertyDetailsComponent {
 	Property: Property | undefined
 	checkIn: any
 	checkOut: any
+	cantDias: number
 
 	constructor() {
 		this.route.queryParams.subscribe((params) => {
@@ -25,5 +26,24 @@ export class PropertyDetailsComponent {
 		this.propertyService
 			.getPropertyById(this.propertyId)
 			.subscribe((data) => (this.Property = data))
+
+		const checkInExp = new Date(this.checkIn)
+		const checkOutExp = new Date(this.checkOut)
+		this.cantDias = this.dateDiffInDays(checkInExp, checkOutExp)
+	}
+
+	calculateTotal(dias: number, price: any) {
+		return dias * price
+	}
+
+	dateDiffInDays(start: Date, end: Date) {
+		const _MS_PER_DAY = 1000 * 60 * 60 * 24
+		const utc1 = Date.UTC(
+			start.getFullYear(),
+			start.getMonth(),
+			start.getDate(),
+		)
+		const utc2 = Date.UTC(end.getFullYear(), end.getMonth(), end.getDate())
+		return Math.floor((utc2 - utc1) / _MS_PER_DAY)
 	}
 }
